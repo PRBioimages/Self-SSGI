de88019de1dd9d9cd725355203c37a245bfb8c4f
Pattern Recognition and Bioimage Informatics Group <84379335+PRBioimages@users.noreply.github.com>
2024-10-30 15:18:06 +0800

    Update model.py

--- 4.Multimodal data fusion/model.py
+++ 4.Multimodal data fusion/model.py
@@ -38,41 +38,17 @@ class TransformerEncoderlayer(nn.Module):
 
 
     def forward(self, y, a):
-        # a: (batch_size, seq_len_a, dim)
-        # b: (batch_size, seq_len_b, dim)
-        # y: (batch_size, seq_len_y, dim)
-
         a = a.permute(1, 0, 2)    # (seq_len_y, batch_size, dim)
         y = y.permute(1, 0, 2)  # (seq_len_y, batch_size, dim)
-
-        # 1. Self-attention on y (y attends to itself)
-        #y_self_attn_output, _ = self.self_attn(y, y, y)  # (seq_len_y, batch_size, dim)
-        #y = y + self.dropout1(y_self_attn_output)  # Residual connection for self-attention
-        #y = self.norm1(y)  # Layer normalization after self-attention
-
-        #a_self_attn_output, _ = self.self_attn(a, a, a)  # (seq_len_y, batch_size, dim)
-        #a = a + self.dropout2(a_self_attn_output)  # Residual connection for self-attention
-        #a = self.norm2(a)  # Layer normalization after self-attention
-
-
-        # Cross-attention from a and b to y
         attn_output, attn_weights = self.cross_attn(y, a)
 
-
-
-
-
-
-        # Residual connection and layer normalization
         y2 = y + self.dropout3(attn_output)
         y2 = self.norm3(y2)  # Apply LayerNorm to the output
 
-        # Feed-forward network
         ff_output = self.linear2(self.dropout(self.linear1(y2)))  # (seq_len_y, batch_size, dim)
         y2 = y2 + self.dropout4(ff_output)
         y2 = self.norm4(y2)  # Final normalization
 
-        # Permute back to the original shape
         y2 = y2.permute(1, 0, 2)  # (batch_size, seq_len_y, dim)
 
         return y2, attn_weights
